6ce3340e896bd49365a3dbed83596c4f886d21c8
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-06-28 15:00:24 +0530

    Time: 0 ms (100%), Space: 60.2 MB (48.16%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxCount = 0;
+
+        for(int right=0; right < nums.size(); right++){
+            if(nums[right] == 0){
+                zeroCount++;
+            }
+            while(zeroCount > 1){
+                if(nums[left] == 0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxCount = max(maxCount , right-left);
+        }
+        return maxCount;
+    }
+};
