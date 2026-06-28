b19f3fcad02535db9dad786e1db34e44915bc440
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-06-28 17:32:25 +0530

    Time: 0 ms (100%), Space: 35.6 MB (79.6%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,35 +4,17 @@ public:
         int n = nums.size();
         int totalsum = 0;
 
-        // calculate total sum
         for(int i=0;i<n;i++){
             totalsum += nums[i];
         }
+        int leftSum = 0;
+        for(int i=0;i<n;i++){
+            int rightSum = totalsum - leftSum - nums[i];
 
-        // built prefix sum array
-        vector<int> prefix(n);
-        prefix[0] = nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-
-        int left = 0, right = 0;
-        for(int index=0;index<n;index++){
-            if(index == 0){
-                left = 0;
-                right = totalsum - left - nums[index];
+            if(leftSum == rightSum){
+                return i;
             }
-            else if(index == n-1){
-                left = prefix[n-2];
-                right = 0;
-            }
-            else{
-                left = prefix[index-1];
-                right = totalsum - left - nums[index];
-            }
-            if(left == right){
-                return index;
-            }
+            leftSum += nums[i];
         }
         return -1;
     }
