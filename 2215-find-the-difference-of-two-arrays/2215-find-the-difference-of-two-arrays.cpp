c8dac22b1fa19e7b17d268bec54db2ef5cc0c3fd
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-06-29 15:35:35 +0530

    Time: 23 ms (54.74%), Space: 37.5 MB (48.86%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        // Store unique elements from both arrays
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
+
         vector<int> ans1;
         vector<int> ans2;
 
-        for(int i = 0; i < nums1.size(); i++){
-            if(find(nums2.begin(), nums2.end(), nums1[i]) == nums2.end() && 
-               find(ans1.begin(), ans1.end(), nums1[i]) == ans1.end()){
-                ans1.push_back(nums1[i]);
-           }
+        // Find elements present in nums1 but not in nums2
+        for(int x : s1){
+            if(s2.find(x) == s2.end()){    // x not found in s2
+                ans1.push_back(x);
+            }
         }
 
-        for(int i = 0; i < nums2.size(); i++){
-            if(find(nums1.begin(), nums1.end(), nums2[i]) == nums1.end() && 
-               find(ans2.begin(), ans2.end(), nums2[i]) == ans2.end()){
-                ans2.push_back(nums2[i]);
-           }
+        // Find elements present in nums2 but not in nums1
+        for(int x : s2){
+            if(s1.find(x) == s1.end()){    // x not found in s1
+                ans2.push_back(x);
+            }
         }
-        return {ans1 , ans2};
+        return {ans1, ans2};
     }
 };
