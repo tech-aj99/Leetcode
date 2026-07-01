72982163c4d21e41affa4ac3b89bf4830d960d97
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-01 18:32:11 +0530

    Time: 206 ms (44.39%), Space: 170.6 MB (54.1%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        map<int ,int> mp;
+        // count losses
+        for(int i=0;i<matches.size();i++){
+            int winner = matches[i][0];
+            int losser = matches[i][1];
+
+            mp[winner] += 0;
+            mp[losser]++;
+        }
+
+        vector<int> zeroloss;
+        vector<int> oneloss;
+
+        // separate players based on lossses
+        for(auto it : mp){
+            if(it.second == 0){
+                zeroloss.push_back(it.first);
+            }
+            else if(it.second == 1){
+                oneloss.push_back(it.first);
+            }
+        }
+        return {zeroloss, oneloss};
+    }
+};
