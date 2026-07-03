9f2bfcbf13cd727f3c7d9a526a39c6c2ad501bf4
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-03 15:06:46 +0530

    Time: 1 ms (17.8%), Space: 14.8 MB (78.92%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -3,17 +3,18 @@ public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string ans1 = "";
         string ans2 = "";
+        
         for(int i=0;i<word1.size();i++){
              ans1 += word1[i];
-            continue;
         }
+
         for(int i=0;i<word2.size();i++){
              ans2 += word2[i];
-            continue;
         }
         if(ans1 == ans2){
             return true;
         }
+
         return false;
     }
 };
