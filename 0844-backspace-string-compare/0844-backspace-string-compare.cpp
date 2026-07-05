3747d23eef7749cd502e13cad95f4a8f0971c7a0
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-05 16:25:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (26.62%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string solve(string s) {
-        for(int i=0;i<s.size();i++){
-            if(s[i] == '#'){
-                if(i == 0){
-                    s.erase(i, 1);
-                }
-                else{
-                    s.erase(i-1, 2);
-                }
-                i=-1;
+        string ans;
+
+        for(char ch : s){
+            if(ch != '#'){
+                ans += ch;
+            }
+            else if(!ans.empty()){
+                ans.pop_back();
             }
         }
-        return s;
+        return ans;
     }
     bool backspaceCompare(string s, string t){
         return solve(s) == solve(t);
