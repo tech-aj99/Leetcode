fe0ad45bd4093941a0e588d1861422c5456d5db1
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-06 16:10:02 +0530

    Time: 240 ms (5.14%), Space: 24 MB (8.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i] != 0){
-                swap(nums[i] , nums[j]);
-                j++;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i] == 0){
+                    swap(nums[i] , nums[j]);
+                }
             }
         }
     }
