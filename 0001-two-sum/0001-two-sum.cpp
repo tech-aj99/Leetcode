1eb46b8a4ff5477db98e9a11d9c41731c1fd496c
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-06 15:27:55 +0530

    Time: 43 ms (24.15%), Space: 14.1 MB (81.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        unordered_map<int, int> mp; // value -> index
-
-        for(int i = 0; i < nums.size(); i++) {
-            int remain = target - nums[i]; // required number
-            // If complement exists
-            if(mp.find(remain) != mp.end()) {
-                return {mp[remain], i}; // return indices
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i] + nums[j] == target){
+                    return {i , j};
+                }
             }
-            mp[nums[i]] = i; // store number and index
         }
-
-        return {}; // no pair found
+        return {};
     }
 };
