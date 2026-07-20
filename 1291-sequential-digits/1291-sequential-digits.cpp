58f68d5f7eba410df083a0e398979b31d31e537d
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-20 13:06:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (14.45%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        queue<int> q;
-
-        // start with single digits
-        for(int i=1;i<=8;i++){
-            q.push(i);
-        }
-
         vector<int> ans;
 
-        while(!q.empty()){
-            int temp = q.front();
-            q.pop();
-        
-        // valid sequential number
-        if(temp >= low && temp <= high){
-            ans.push_back(temp);
-        }
-        int last_digit = temp % 10; // Get last digit
+        for (int start = 1; start <= 9; start++) {
 
-        // Append last digit
-        if(last_digit + 1 <= 9){
-            q.push(temp*10 + (last_digit + 1));
+            int num = start;
+
+            for (int next = start + 1; next <= 9; next++) {
+
+                num = num * 10 + next;
+
+                if (num >= low && num <= high) {
+                    ans.push_back(num);
+                }
+            }
         }
-      }
-      return ans;
+
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
