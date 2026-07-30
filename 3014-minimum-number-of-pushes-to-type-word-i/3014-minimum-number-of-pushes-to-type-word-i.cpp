046155084c995f9971af1b44de4e8f6dd97e55af
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-07-30 09:28:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
+    int getValue(int i){
+        if(i < 8){
+            return 1;
+        }
+        else if(i < 16){
+            return 2;
+        }
+        else if(i < 24){
+            return 3;
+        }
+        else {
+            return 4;
+        }
+    }
+
     int minimumPushes(string word) {
         int ans = 0;
-       for(int i=0;i<word.size();i++){
-        ans += (i / 8) + 1;
-       } 
-       return ans;
+        for(int i=0;i<word.size();i++){
+            ans += getValue(i);
+        }
+        return ans;
     }
 };
