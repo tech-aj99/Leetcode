4ec10fa45e0a4c89139016e33d837a1aac1aa3c0
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-08-05 14:02:18 +0530

    Time: 2 ms (36.21%), Space: 10.9 MB (27.62%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+
+    void solve(vector<int>& nums, int index){
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+
+        for(int i =index;i<nums.size();i++){
+            swap(nums[index], nums[i]);
+            solve(nums, index+1);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums, 0);
+        return ans;
+    }
+};
