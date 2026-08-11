acb84f9dd43ddb6ac43541175944dc61523be1a8
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-08-11 15:23:03 +0530

    Time: 258 ms (51.1%), Space: 10.7 MB (55.2%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board, string word, int row, int col, int i){
+    bool solve(vector<vector<char>>& board, string &word, vector<vector<int>> &visited, int row, int col, int i){
         if(i == word.size()) return true;
 
         if(row < 0 || row >= board.size() ||
@@ -8,27 +8,33 @@ public:
             return false;
            }
         
-        if(board[row][col] != word[i])  return false;
+        if(visited[row][col] || board[row][col] != word[i])  return false;
 
-        char temp = board[row][col];
-        board[row][col] = '#';
+        visited[row][col] = 1;
 
-        bool found = solve(board, word, row+1, col, i+1) || 
-                     solve(board, word, row-1, col, i+1) ||
-                     solve(board, word, row, col+1, i+1) || 
-                     solve(board, word, row, col-1, i+1);
+        if(solve(board, word, visited, row+1, col, i+1) || 
+                     solve(board, word, visited, row-1, col, i+1) ||
+                     solve(board, word, visited, row, col+1, i+1) || 
+                     solve(board, word, visited, row, col-1, i+1)){
+                        return true;
+                     }
 
-        board[row][col] = temp;
+        visited[row][col] = 0;
 
-        return found;
+        return false;
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        for(int row = 0;row < board.size();row++){
-            for(int col = 0;col < board[0].size();col++){
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        for(int row = 0;row < n;row++){
+            for(int col = 0;col < m;col++){
 
                 if(board[row][col] == word[0]){
-                    if(solve(board, word, row, col, 0))
+                    if(solve(board, word, visited, row, col, 0))
                         return true;
                 }
             }
