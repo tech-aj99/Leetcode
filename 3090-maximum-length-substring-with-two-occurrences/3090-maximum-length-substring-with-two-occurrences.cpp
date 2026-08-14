079b75d7a4324c38b55df507112721905bbf14d5
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-08-14 12:04:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (95.5%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
+        int left = 0;
         int maxi = 0;
+        vector<int> freq(26, 0);
 
-        for(int left = 0;left < s.size();left++){
-            for(int right = 0;right < s.size();right++){
-                string strtemp = s.substr(left, right-left+1);
-                
-                unordered_map<char, int> mp;
-                for(char ch : strtemp){
-                    mp[ch]++;
-                }
-                bool valid = true;
+        for(int right = 0;right < s.size(); right++){
+            freq[s[right] - 'a']++;
 
-                for(auto it : mp){
-                    if(it.second > 2){
-                        valid = false;
-                        break;
-                    }
-                }
-                if(valid){
-                    maxi = max(maxi, (int)strtemp.size());
-                }
+            while(freq[s[right] - 'a'] > 2){
+                freq[s[left] - 'a']--;
+                left++;
             }
+            maxi = max(maxi, right - left + 1);
         }
         return maxi;
     }
