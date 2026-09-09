29ab52794168a7e32943ef3a5d4f0952f695b449
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-09-09 12:28:07 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,49 +1,20 @@
 class Solution {
 public:
     long long countCommas(long long n) {
+        long long ans = 0;
+        long long start = 1000;
+        long long commas = 1;
 
-        if(n >= 1 && n <= 999){
-            return 0;
+        while(start <= n){
+            long long end = start * 1000 - 1;
+            if(n < end){
+                end = n;
+            }
+
+            ans += (end - start + 1) * commas;
+            start = start * 1000;
+            commas++;
         }
-
-        else if(n >= 1000 && n <= 999999){
-            long long ans = n - 1000 + 1;
-            return ans * 1;
-        }
-
-        else if(n >= 1000000 && n <= 999999999){
-            long long ans1 = 999999 - 1000 + 1;
-            long long ans2 = n - 1000000 + 1;
-
-            return ans1 + (ans2 * 2);
-        }
-
-        else if(n >= 1000000000 && n <= 999999999999){
-            long long ans1 = 999999 - 1000 + 1;
-            long long ans2 = 999999999 - 1000000 + 1;
-            long long ans3 = n - 1000000000 + 1;
-
-            return ans1 + (ans2 * 2) + (ans3 * 3);
-        }
-
-        else if(n >= 1000000000000 && n <= 999999999999999){
-            long long ans1 = 999999 - 1000 + 1;
-            long long ans2 = 999999999 - 1000000 + 1;
-            long long ans3 = 999999999999 - 1000000000 + 1;
-            long long ans4 = n - 1000000000000 + 1;
-
-            return ans1 + (ans2 * 2) + (ans3 * 3) + (ans4 * 4);
-        }
-
-        else {
-            long long ans1 = 999999 - 1000 + 1;
-            long long ans2 = 999999999 - 1000000 + 1;
-            long long ans3 = 999999999999 - 1000000000 + 1;
-            long long ans4 = 999999999999999 - 1000000000000 + 1;
-            long long ans5 = n - 1000000000000000 + 1;
-
-            return ans1 + (ans2 * 2) + (ans3 * 3)
-                         + (ans4 * 4) + (ans5 * 5);
-        }
+        return ans;
     }
 };
