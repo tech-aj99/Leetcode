ecd3861a83704eee724763b4909aeead08556ca5
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-09-11 13:49:50 +0530

    Time: 14 ms (26.4%), Space: 31 MB (51.66%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    int totalNumbers(vector<int>& digits) {
-        int n = digits.size();
-        int count = 0;
-        set<int> s;
-        
-        for(int i=0;i<n;i++){
-            
-            if(digits[i] == 0){
+    void solve(vector<int>& digits, vector<bool> &used, int num, int count, set<int> &s){
+        if(count == 3){
+            if(num % 2 == 0){
+                s.insert(num);
+            }
+            return;
+        }
+
+        for(int i=0;i<digits.size();i++){
+            if(used[i]) continue;
+
+            if(count == 0 && digits[i] == 0){
                 continue;
             }
 
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    if(i != j && j != k && k != i){
-                        int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+            used[i] = true;
 
-                        if(num % 2 == 0){
-                            s.insert(num);
-                        }
-                    }
-                }
-            }
+            solve(digits, used, num * 10 + digits[i], count+1, s);
+
+            used[i] = false;
         }
+    }
+
+    int totalNumbers(vector<int>& digits) {
+        int n = digits.size();
+
+        vector<bool>used(n, false);
+        set<int> s;
+
+        solve(digits, used, 0, 0, s);
+
         return s.size();
     }
 };
