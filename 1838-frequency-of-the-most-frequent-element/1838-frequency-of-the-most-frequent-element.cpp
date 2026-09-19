9acce7016d925d448c19dc220e30115f0bfcc478
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-09-19 12:50:41 +0530

    Time: 92 ms (7.7%), Space: 115.3 MB (5.31%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int bSearch(int target_idx, int k, vector<int>& nums, vector<long long>& prefixSum){
+        int target = nums[target_idx];
+
+        int l = 0;
+        int r = target_idx;
+
+        int best_idx = target_idx;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            
+            long long count = target_idx - mid + 1;
+            long long windowSum = count * target;
+
+            long long currSum = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
+
+            long long ops = windowSum - currSum;
+
+            if(ops > k){
+                l = mid + 1;
+            }
+
+            else{
+                best_idx = mid;
+                r = mid - 1;
+            }
+        }
+        return target_idx - best_idx + 1;
+    }
+
+    int maxFrequency(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        vector<long long> prefixSum(n);
+        prefixSum[0] = nums[0];
+
+        for(int i=1;i<n;i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+
+        int result = 0;
+
+        for(int target_idx = 0; target_idx < n; target_idx++){
+            result = max(result, bSearch(target_idx, k, nums, prefixSum));
+        }
+
+        return result;
+    }
+};
