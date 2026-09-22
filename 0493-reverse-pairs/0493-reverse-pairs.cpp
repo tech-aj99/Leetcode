ca11cf9131487e4fedb74ed0d5e3c9709cd624fe
PARALE AJAY DIGAMBAR <ajayparale9@gmail.com>
2026-09-22 14:13:41 +0530

    Time: 575 ms (12.37%), Space: 243.4 MB (44.23%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int low, int mid, int high){
+        int count = 0;
+        int j = mid+1;
+
+        for(int i=low; i<=mid; i++){
+            while(j <= high && (long long)nums[i] > 2LL * nums[j]){
+                j++;
+            }
+            count += j - (mid+1);
+        }
+        return count;
+    }
+
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+
+        int i = low;
+        int j = mid+1;
+
+        while(i <= mid && j <= high){
+            if(nums[i] <= nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else{
+                temp.push_back(nums[j]);
+                j++;
+            }
+        }
+
+        while(i <= mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
+
+        while(j <= high){
+            temp.push_back(nums[j]);
+            j++;
+        }
+
+        for(int k = low; k <= high; k++){
+            nums[k] = temp[k - low];
+        }
+    }
+
+    int mergeSort(vector<int>& nums, int low, int high){
+        if(low >= high){
+            return 0;
+        }
+        int count = 0;
+
+        int mid = low + (high - low) / 2;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid+1, high);
+
+        count += countPairs(nums, low, mid, high);
+
+        merge(nums, low, mid, high);
+
+        return count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size()-1);
+    }
+};
